c62a700257f8db35cf8135a9006e43235b42e7ca
Montasser Mossallem <moun2030@gmail.com>
2017-11-13 12:54:25 +0300

    inprogression tab

--- Ui-Design/user-2/js/appUpdateUx.js
+++ Ui-Design/user-2/js/appUpdateUx.js
@@ -13,8 +13,44 @@ $(document).ready(function(){
      });
 
      //-------------------------------------------------------
-     // Image Uploader handler !!
+     // Enable description Handler // Comment this func once working with angularjs
      //-------------------------------------------------------
-     
+    //  $('.description-enable-handler').on('click' , function (){
+    //    var $targetTextarea = $(this).next('textarea');
+    //    var $targetCheckbox = $(this).children('div').children('input');
+    //    if($targetCheckbox.hasClass('enabled'))
+    //    {
+    //      $targetTextarea.removeAttr('disabled');
+    //      $targetCheckbox.removeClass('enabled');
+    //    }else{
+    //      $targetTextarea.attr('disabled');
+    //      $targetCheckbox.addClass('enabled');
+    //    }
+    //    console.log($targetTextarea.attr('class'));
+     //
+    //  });
+
+    // ----------------------------------------------------------------
+    //---> Tab Slider
+    // ---------------------------------------------------------------
+    /*  Init The Hiden and display for tabs */
+    // $(".tab-slider--body").hide();
+    // $(".tab-slider--body:first").show();
+    $(".tab-slider--body").show();
+    $(".tab-slider--body:first").hide();
+    // Do an action for tab ( 1 => editor ) and tab ( 2=> Settings )
+    $(".tab-slider--nav li").click(function() {
+        $(".tab-slider--body").hide();
+        var activeTab = $(this).attr("rel");
+        $("#"+activeTab).fadeIn();
+        if($(this).attr("rel") == "tab2"){
+        $('.tab-slider--tabs').addClass('slide');
+        }else{
+        $('.tab-slider--tabs').removeClass('slide');
+        }
+        $(".tab-slider--nav li").removeClass("active");
+        $(this).addClass("active");
+    });
+
 
 });
