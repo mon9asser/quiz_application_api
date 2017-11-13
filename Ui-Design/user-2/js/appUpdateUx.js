3f56564dbe7beeeb7e06a59d4c8f8dd5f3276c4c
Montasser Mossallem <moun2030@gmail.com>
2017-11-13 09:15:06 +0300

    video and image not clear -- update

--- Ui-Design/user-2/js/appUpdateUx.js
+++ Ui-Design/user-2/js/appUpdateUx.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
-  
+
+     //-------------------------------------------------------
+     // Video Url Editor !!
+     //-------------------------------------------------------
+     $('.youtubeurl-handler').on('click' , function(){
+        var target = $($(this).attr('data-target-to'));
+        if(target.css('display') =='none')
+        target.css('display','block');
+        else {
+          target.css('display','none');
+        }
+     });
+
+     //-------------------------------------------------------
+     // Image Uploader handler !!
+     //-------------------------------------------------------
+     
+
 });
