b8440a996d5106ee98dee4b3061b2a3cee22dc7c
root <montasser mossallem>
2018-05-01 14:37:34 +0200

    TObject

--- ui-public/ext/js/app-apps.js
+++ ui-public/ext/js/app-apps.js
@@ -1083,7 +1083,7 @@ apps.controller("apps-controller" , ['$scope','$http' , '$timeout','$window','$r
 
       question_selected.answers_format.push(new_answer);
       $scope.iframe_access.add_data_to_view($scope.question_id , new_answer);
-      $scope.update_settings_in_view();
+
     };
     $scope.question_answer_deletion = function (answer_id){
 
@@ -2265,6 +2265,7 @@ apps.controller("apps-controller" , ['$scope','$http' , '$timeout','$window','$r
             $(".editor-container").css({display: 'none'});
             $(".preview-container").css({display: 'block'});
             $('.question-editor , .question-preview').removeClass("overflow_hidden");
+            $scope.update_settings_in_view();
           } , 200 );
         }
 
