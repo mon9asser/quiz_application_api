ee849de298367fd1d5aa1aa04bcb520a5530dff2
root <montasser mossallem>
2018-06-02 18:48:18 +0200

    fix app controller

--- database/config.js
+++ database/config.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const _ = require("lodash");
 var config = {
-    server_ip:             'http://localhost:9000/'  , // http://34.215.133.182/
+    server_ip:             'http://localhost:9000/'  , // http://54.187.251.111/
     port :                  ':27017' ,
     database_name :         '/todo' ,
     host_name:              'mongodb://localhost' ,
