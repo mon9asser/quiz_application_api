9e4649a8ba976d354dcb090aa6b284c2d1f242e6
root <montasser mossallem>
2018-07-09 19:25:48 +0300

    newIssue

--- server/routes/rpt.js
+++ server/routes/rpt.js
@@ -2588,8 +2588,11 @@ rptRouters.post("/:app_id/detailed/report", api_key_report_auth ,( req , res ) =
 
 
                if( req.body.questions != null && req.body.questions == true ){
-                    if( an_online_report.attendee_questions != undefined || an_online_report.attendee_questions.length == 0){
+                    if( an_online_report.attendee_questions != undefined  ){
+                      if(an_online_report.attendee_questions.length != 0 )
                       detailes_report.items['questions'] = an_online_report.attendee_questions
+                      else
+                      detailes_report.items['questions'] ="No questions meet your selected criteria" ;
                     }else {
                       detailes_report.items['questions'] ="No questions meet your selected criteria"
                     }
@@ -2645,12 +2648,13 @@ rptRouters.post("/:app_id/detailed/report", api_key_report_auth ,( req , res ) =
                 attendee_object['completed_date'] = ( detailed_online_report != undefined ) ?  detailed_online_report.completed_date : "unkonwn date";
 
                  if( req.body.questions != null && req.body.questions == true ){
-                      if( an_online_rpt.attendee_questions != undefined || an_online_rpt.attendee_questions.length == 0){
-                        console.log(an_online_rpt.attendee_questions);
-                        console.log("====================================");
+                      if( an_online_rpt.attendee_questions != undefined ){
+                        if ( an_online_rpt.attendee_questions.length != 0 )
                         attendee_object['questions'] = an_online_rpt.attendee_questions;
+                        else
+                        attendee_object['questions'] = "No questions meet your selected criteria"
                       }else {
-                        console.log("issue here +++++++++++++++");
+                        if ( an_online_rpt.attendee_questions.length == 0 )
                         attendee_object['questions'] = "No questions meet your selected criteria"
                       }
                  }
