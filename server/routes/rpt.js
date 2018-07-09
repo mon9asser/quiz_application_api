df952bd3e4850009c5e8f373f78e6a42a2db490f
root <montasser mossallem>
2018-07-09 13:06:00 +0300

    issue in question prop

--- server/routes/rpt.js
+++ server/routes/rpt.js
@@ -2581,7 +2581,7 @@ rptRouters.post("/:app_id/detailed/report", api_key_report_auth ,( req , res ) =
 
                if( req.body.questions != null && req.body.questions == true ){
                     if( an_online_report.attendee_questions != undefined || an_online_report.attendee_questions.length == 0){
-                    //  detailes_report.items['questions'] = an_online_report.attendee_questions
+                      detailes_report.items['questions'] = an_online_report.attendee_questions
                     }else {
                       detailes_report.items['questions'] ="No questions meet your selected criteria"
                     }
@@ -2630,7 +2630,7 @@ rptRouters.post("/:app_id/detailed/report", api_key_report_auth ,( req , res ) =
 
                  if( req.body.questions != null && req.body.questions == true ){
                       if( an_online_rpt.attendee_questions != undefined || an_online_rpt.attendee_questions.length == 0){
-                        attendee_object['questions'] = an_online_rpt.attendee_questions
+                      //  attendee_object['questions'] = an_online_rpt.attendee_questions
                       }else {
                         attendee_object['questions'] ="No questions meet your selected criteria"
                       }
