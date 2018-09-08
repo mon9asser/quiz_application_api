fa0fcd39d181006532b896d87b07654cb3a57f3d
Montasser Mossallem <moun2030@gmail.com>
2018-09-08 20:29:34 +0300

    new Updates

--- ui-public/ext/js/quiz-app-quizzes.js
+++ ui-public/ext/js/quiz-app-quizzes.js
@@ -1,27 +1,27 @@
-apps.filter("user_name" , [
-  '$http', 'settings',
-  function ($http , settings){
-    return function (user_id){
-      var json_source =  settings.server_ip + settings.json_source;
-      var user_name = '';
-      $.getJSON( json_source , function(keys_object){
-          $http({
-            url : settings.server_ip + 'api/user/'+ user_id,
-            type : "GET" ,
-            headers: {
-              "X-api-keys": keys_object.API_KEY,
-              "X-api-app-name": keys_object.APP_NAME
-            }
-          }).then(function(response){
-             user_name = response.data.email;
-          } , function (err){
-            console.log(err);
-          });
-      });
-      return user_name ;
-    }
-  }
-]);
+// apps.filter("user_name" , [
+//   '$http', 'settings',
+//   function ($http , settings){
+//     return function (user_id){
+//       var json_source =  settings.server_ip + settings.json_source;
+//       var user_name = '';
+//       $.getJSON( json_source , function(keys_object){
+//           $http({
+//             url : settings.server_ip + 'api/user/'+ user_id,
+//             type : "GET" ,
+//             headers: {
+//               "X-api-keys": keys_object.API_KEY,
+//               "X-api-app-name": keys_object.APP_NAME
+//             }
+//           }).then(function(response){
+//              user_name = response.data.email;
+//           } , function (err){
+//             console.log(err);
+//           });
+//       });
+//       return user_name ;
+//     }
+//   }
+// ]);
 apps.controller('list-apps' , [
   '$scope' , '$rootScope' , '$timeout' , '$http' , 'settings',
   function ($scope, $rootScope, $timeout , $http , settings ){
