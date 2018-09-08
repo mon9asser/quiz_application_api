6c8aa03bdc91c36a08ed0cf3d1659e6e1149652e
Montasser Mossallem <moun2030@gmail.com>
2018-09-08 20:45:48 +0300

    new Updates

--- ui-public/ext/js/quiz-app-quizzes.js
+++ ui-public/ext/js/quiz-app-quizzes.js
@@ -3,11 +3,12 @@ apps.controller("list-apps" , [
   $scope.server_ip = settings.server_ip ;
   $scope.api_url =  $scope.server_ip + 'api/applications/list/data' ;
   $scope._applications_ = [] ;
+  console.log($scope.api_url);
   $http({
     method : "GET" ,
     url : $scope.api_url
   }).then(( response ) => {
-    $scope._applications_ = response.data ; 
+    $scope._applications_ = response.data ;
   }).catch((err)=>{
     console.log(err);
   });
