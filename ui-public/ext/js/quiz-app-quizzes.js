87efb586a57e8830ebd1e7db85c5409d015a6939
Montasser Mossallem <moun2030@gmail.com>
2018-09-08 20:42:19 +0300

    new Updates

--- ui-public/ext/js/quiz-app-quizzes.js
+++ ui-public/ext/js/quiz-app-quizzes.js
@@ -1,4 +1,14 @@
 apps.controller("list-apps" , [
-'$scope' , '$rootScope' , ( $scope , $rootScope ) => {
-  alert();
+'$scope' , '$rootScope' , '$timeout' , '$http' , 'settings' , ( $scope , $rootScope , $timeout , $http , settings ) => {
+  $scope.server_ip = settings.server_ip ;
+  $scope.api_url =  $scope.server_ip + 'api/applications/list/data' ;
+  $scope._applications_ = [] ;
+  $http({
+    method : "GET" ,
+    url : $scope.api_url
+  }).then(( response ) => {
+    $scope._applications_ = response.data ; 
+  }).catch((err)=>{
+    console.log(err);
+  });
 }]);
